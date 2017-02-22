f1e95ec320ffd12f4321e4ccac67de4e4109d26c
codelikeleon <leonschiedermair@hotmail.co.uk>
2017-02-22 14:35:56 +0000

    Initial Commit

---
+++ OpenGLScene/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLScene
+//
+//  Created by Leon Schiedermair on 22/02/2017.
+//  Copyright © 2017 Leon Schiedermair. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
